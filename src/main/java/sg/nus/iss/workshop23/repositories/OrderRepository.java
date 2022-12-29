1d44635c2b50a6737a62b0829567d83e74d9b9dc
JJ How <holyyamateh@hotmail.sg>
2022-12-29 15:48:37 +0800

    test

---
+++ src/main/java/sg/nus/iss/workshop23/repositories/OrderRepository.java
@@ -0,0 +1,29 @@
+package sg.nus.iss.workshop23.repositories;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
+
+import sg.nus.iss.workshop23.models.OrderDetails;
+
+@Repository
+public class OrderRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public OrderDetails getDiscountedOrderDetails(Integer orderId) {
+        // prevent inheritance
+        final List<OrderDetails> ordDetails = new LinkedList<>();
+        // perform the query
+        final SqlRowSet rs = jdbcTemplate.queryForRowSet(SQL_GET_ORDER_DETAILS_WITH_DISCOUNT, orderId);
+
+        while (rs.next()) {
+            ordDetails.add(OrderDetails.create(rs));
+        }
+        return ordDetails.get(0);
+    }
+}
